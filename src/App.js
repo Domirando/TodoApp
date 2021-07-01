fb991f27f4213759a38b406b153af3bd4c946584
Maftuna1707 <72442735+Maftuna1707@users.noreply.github.com>
2021-07-01 22:59:19 +0500

    Getting todos from server

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
 import TodoList from './Todo/TodoList';
 import AddTodos from './Todo/AddTodos';
 import Context from './context';
 
 function App() {
-  const [todos, setTodos] = React.useState(
-    [
-    {id: 1, completed: false, title: 'Internship'},
-    {id: 2, completed: false, title: 'Increase coding'},
-    {id: 3, completed: false, title: 'Practice with coding'},
-    {id: 4, completed: false, title: 'Housework activities'},
-    {id: 5, completed: false, title: 'Look after to siblings'}, 
-    {id: 6, completed: false, title: 'Teach siblings'} 
-  ]
-  );
+  const [todos, setTodos] = React.useState([]);
+  useEffect(() => {
+    fetch('https://jsonplaceholder.typicode.com/todos?_limit=6')
+      .then(response => response.json())
+      .then(todos => {
+        setTimeout(() => {
+          setTodos(todos)
+        }, 2000)
+      })
+  }, [])
   function toggleTodo(id){
     setTodos(todos.map(todo => {
       if (todo.id === id) {
