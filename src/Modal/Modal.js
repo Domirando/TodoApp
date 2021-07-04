73f11e15ac495a531a1cdf2f95775a6d00fa3053
Vohidjonovna <maftunavohidjonovna@gmail.com>
2021-07-04 22:59:16 +0500

    Changed

--- src/Modal/Modal.js
+++ src/Modal/Modal.js
@@ -8,14 +8,14 @@ export default class Modal extends React.Component{
   render(){
     return(
       <React.Fragment>
-        <button onClick={() => this.setState({isOpen: true})}>Open modal</button>
+        <button className="btn" onClick={() => this.setState({isOpen: true})}>Open modal</button>
 
         {this.state.isOpen && (
         <div className="modal">
           <div className="modal-body">
             <h1>Modal window</h1>
             <p>lorem ipsum</p>
-            <button onClick={() => this.setState({isOpen: false})}>Close modal</button>
+            <button  className="btn" onClick={() => this.setState({isOpen: false})}>Close modal</button>
           </div>
         </div>)
         }
